35424ebd29ce8c49d5d558629d0a55856bf89acd
gregwhitaker <gwhitake@gmail.com>
2019-06-01 13:38:48 -0700

    Doing work

--- inventory-service/src/main/java/demo/inventory/Application.java
+++ inventory-service/src/main/java/demo/inventory/Application.java
@@ -15,16 +15,13 @@
  */
 package demo.inventory;
 
-import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String... args) {
-        new SpringApplicationBuilder(Application.class)
-                .web(WebApplicationType.SERVLET)
-                .run(args);
+        SpringApplication.run(Application.class, args);
     }
 }
