1561b91ead382b970caedc6d98044d293f3434be
gregwhitaker <gwhitake@gmail.com>
2019-06-01 15:28:35 -0700

    Doing work

--- inventory-service/src/main/java/demo/inventory/service/error/ProductNotFoundException.java
+++ inventory-service/src/main/java/demo/inventory/service/error/ProductNotFoundException.java
@@ -1,10 +1,15 @@
 package demo.inventory.service.error;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class ProductNotFoundException extends RuntimeException {
 
     private final String productId;
 
     public ProductNotFoundException(final String productId) {
+        super(String.format("Product Not Found [productId: %s]", productId));
         this.productId = productId;
     }
 
--- inventory-service/src/main/java/demo/inventory/service/error/SkuNotFoundException.java
+++ inventory-service/src/main/java/demo/inventory/service/error/SkuNotFoundException.java
@@ -1,10 +1,15 @@
 package demo.inventory.service.error;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class SkuNotFoundException extends RuntimeException {
 
     private final String sku;
 
     public SkuNotFoundException(final String sku) {
+        super(String.format("Sku Not Found [sku: %s]", sku));
         this.sku = sku;
     }
 
