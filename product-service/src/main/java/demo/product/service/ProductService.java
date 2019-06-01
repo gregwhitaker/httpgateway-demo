b383244577d87867c6c6bd09f1d5065caf3fc3c9
gregwhitaker <gwhitake@gmail.com>
2019-06-01 15:58:48 -0700

    Doing work

--- product-service/src/main/java/demo/product/service/ProductService.java
+++ product-service/src/main/java/demo/product/service/ProductService.java
@@ -1,12 +1,26 @@
 package demo.product.service;
 
+import demo.product.service.model.ProductInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class ProductService {
     private static final Logger LOG = LoggerFactory.getLogger(ProductService.class);
 
+    private final Map<String, ProductInfo> productInfos = new HashMap<>();
 
+    public ProductInfo getProductInfo(String productId) {
+        return null;
+    }
+
+    @PostConstruct
+    public void init() {
+        
+    }
 }
