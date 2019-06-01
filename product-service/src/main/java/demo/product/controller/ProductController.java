9c4dc1ae78211312310464c0e2d3dab96f59e7a3
gregwhitaker <gwhitake@gmail.com>
2019-06-01 14:40:43 -0700

    Doing work

--- product-service/src/main/java/demo/product/controller/ProductController.java
+++ product-service/src/main/java/demo/product/controller/ProductController.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,7 +13,7 @@ public class ProductController {
     private static final Logger LOG = LoggerFactory.getLogger("netifi-demo");
 
     @GetMapping("/product/{productId}")
-    public ResponseEntity<ProductInfoResponse> getProductInfo() {
+    public ResponseEntity<ProductInfoResponse> getProductInfo(@PathVariable("productId") String productId) {
         return null;
     }
 }
